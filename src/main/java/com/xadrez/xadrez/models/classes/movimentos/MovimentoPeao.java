db4e04fe30279705c95d278e5f0481f60852c953
Weberttz <johngaldencio18@gmail.com>
2026-07-09 00:39:24 -0300

    fix: ajusta movimento do peão, ele estava voltando para trás

--- src/main/java/com/xadrez/xadrez/models/classes/movimentos/MovimentoPeao.java
+++ src/main/java/com/xadrez/xadrez/models/classes/movimentos/MovimentoPeao.java
@@ -7,10 +7,13 @@ import com.xadrez.xadrez.models.interfaces.EstrategiaMovimento;
 public class MovimentoPeao implements EstrategiaMovimento {
     @Override
     public boolean isMovimentoValido(Posicao origem, Posicao destino, Cor cor) {
-        int distanciaX = Math.abs(origem.getX() - destino.getX());
-        int distanciaY = Math.abs(origem.getY() - destino.getY());
+        int distanciaX = origem.getX() - destino.getX();
+        int distanciaY = origem.getY() - destino.getY();
 
-        //boolean naoMoveuAinda = (origem.getX() == 1 || origem.getX() == 6);
+        if(cor.equals(Cor.PRETA)) {
+            distanciaX *= -1;
+            distanciaY *= -1;
+        }
 
         return distanciaX == 1 || distanciaX == 2 || (distanciaX + distanciaY) == 2;
     }
